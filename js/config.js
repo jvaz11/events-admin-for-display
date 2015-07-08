c55af2d75bac2c168b080042d5e035943b08a7b7
Juan Vazquez <juanvme123@gmail.com>
2015-07-08 00:04:46 -0700

    User authentication
    Now, anyone can have their own events board!

--- js/config.js
+++ js/config.js
@@ -1,39 +1,133 @@
-function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
-    $urlRouterProvider.otherwise("/index/events");
+function config($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'     
+      })
+      .when('/settings', {
+        templateUrl: 'views/settings.html',
+        controller: 'MainCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/events', {
+        templateUrl: 'views/events.html',
+        controller: 'MainCtrl',
+        resolve: {
+          currentAuth: function(Auth) {
+            return Auth.requireAuth();
+          }
+        }
+      })
+    
+      //   resolve: {
+      //     currentAuth: function(Auth) {
+      //       return Auth.requireAuth();
+      //     }
+      //   }
+      // })
+      .otherwise({
+        redirectTo: '/login'
+      })
+  };
 
-    $ocLazyLoadProvider.config({
-        // Set to true if you want to see what and when is dynamically loaded
-        debug: true
-    });
 
-    $stateProvider
 
-        .state('index', {
-            abstract: true,
-            url: "/index",
-            templateUrl: "views/common/content.html",
-        })
-        .state('index.display', {
-            url: "/display",
-            templateUrl: "views/display.html",
-            data: { pageTitle: 'Example view' }
-        })
-        .state('index.events', {
-            url: "/events",
-            templateUrl: "views/events.html",
-            data: { pageTitle: 'Events' },
-            controller: MainCtrl
-        })
-        .state('index.settings', {
-            url: "/settings",
-            templateUrl: "views/events.html",
-            data: { pageTitle: 'Settings' },
-            controller: MainCtrl
-        })
-}
+
 angular
     .module('eventsBoard')
     .config(config)
-    .run(function($rootScope, $state) {
-        $rootScope.$state = $state;
+     .run["$rootScope", "$location", (function($rootScope, $location) {
+    $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+      // We can catch the error thrown when the $requireAuth promise is rejected
+      // and redirect the user back to the login page
+      if (error === "AUTH_REQUIRED") {
+        $location.path("/login");
+      }
     });
+  })]  
+
+
+// function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+//     $urlRouterProvider.otherwise("/login");
+
+//     $ocLazyLoadProvider.config({
+//         // Set to true if you want to see what and when is dynamically loaded
+//         debug: true
+//     });
+
+//     $stateProvider
+
+//         .state('index', {
+//             abstract: true,
+//             url: "/index",
+//             templateUrl: "views/common/content.html"
+//         })
+//         .state('index.display', {
+//             url: "/display",
+//             templateUrl: "views/display.html",
+//             data: {
+//                 pageTitle: 'Example view'
+//             }
+//         })
+//         .state('index.events', {
+//             url: "/events",
+//             templateUrl: "views/events.html",
+//             data: {
+//                 pageTitle: 'Events'
+//             },
+//             controller: MainCtrl
+//         })
+//         .state('index.settings', {
+//             url: "/settings",
+//             templateUrl: "views/events.html",
+//             data: {
+//                 pageTitle: 'Settings'
+//             },
+//             controller: MainCtrl
+//         })
+//         .state('register', {
+//             url: "/register",
+//             templateUrl: "views/register.html",
+//             data: {
+//                 pageTitle: 'Register',
+//                 specialClass: 'gray-bg'
+//             },
+//             controller: AuthCtrl
+//         })
+//         .state('login', {
+//             url: "/login",
+//             templateUrl: "views/login.html",
+//             data: {
+//                 pageTitle: 'Login',
+//                 specialClass: 'gray-bg'
+//             },
+//             controller: AuthCtrl
+//         })
+// }
+// angular
+//     .module('eventsBoard')
+//     .config(config)
+//     .run(function($rootScope, $state) {
+//         $rootScope.$state = $state;
+//     });
+
+
+
+
+// // old .run
+// // .run(["$rootScope", "$state", "$location", function($rootScope, $state, $location) {
+// //     $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+// //         // We can catch the error thrown when the $requireAuth promise is rejected
+// //         // and redirect the user back to the home page
+// //         if (error === "AUTH_REQUIRED") {
+// //             $state.go("login");
+// //         }
+// //     });
+// // }]);
---
+++ js/controllers/authController.js
@@ -0,0 +1,77 @@
+function AuthCtrl($scope, $location, toaster, Auth) {
+    
+
+  if(Auth.signedIn()) {
+    console.log(Auth);
+    $location.path('/events');
+  }
+
+    $scope.register = function(user) {          
+    Auth.register(user)
+      .then(function() {
+        toaster.pop('success', "Registered successfully");
+        $location.path('/events');
+      }, function(err) {
+        errMessage(err);
+      });
+  };
+
+    $scope.login = function(user) {
+     Auth.login(user)
+      .then(function() {
+        toaster.pop('success', "Logged in successfully");
+        $location.path('/events');
+      }, function(err) {        
+        errMessage(err);
+      });    
+    };
+
+    $scope.changePassword = function(user) {
+     Auth.changePassword(user)
+      .then(function() {                        
+        
+        // Reset form
+        $scope.email = '';
+        $scope.oldPass = '';
+        $scope.newPass = '';
+
+        toaster.pop('success', "Password changed successfully");
+      }, function(err) {
+        errMessage(err);      
+      });
+  };
+
+  $scope.logout = function() {    
+    Auth.logout();    
+    toaster.pop('success', "Logged out successfully");
+    $location.path('/login');
+  };
+
+    function errMessage(err) {
+
+    var msg = "Unknown Error...";
+
+    if(err && err.code) {
+      switch (err.code) {
+        case "EMAIL_TAKEN": 
+          msg = "This email has been taken"; break;          
+        case "INVALID_EMAIL": 
+          msg = "Invalid email"; break;          
+        case "NETWORK_ERROR": 
+          msg = "Network error"; break;          
+        case "INVALID_PASSWORD": 
+          msg = "Invalid password"; break;          
+        case "INVALID_USER":
+          msg = "Invalid user"; break;                  
+      } 
+    }   
+
+    toaster.pop('error', msg);
+  };
+}
+
+
+
+angular
+    .module('eventsBoard')
+    .controller('AuthCtrl', AuthCtrl)
